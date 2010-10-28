d5e60d4daee2b62fecbe5be9e0aa681dd27dc8d1
Guillaume Aubert <guillaume.aubert@gmail.com>
2010-10-28 13:49:50 +0200

    add run_export script

---
+++ run_export.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+export MDEXPORTER_HOME=/cygdrive/h/tmp/md-exporter
+
+cd $MDEXPORTER_HOME/bin
+
+./md-exporter --rdb ../../../Dev/projects/vnavigator-content/VNAVIGATOR-RDB --out ../../../UPDATES/export-1
+
